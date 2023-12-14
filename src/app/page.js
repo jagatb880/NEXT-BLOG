ef0aa7f0e20a2dadbd84b737cba268c2ab8d4079
jagat.sahoo <jagat.sahoo@robosoftin.com>
2023-12-14 16:08:51 +0530

    updated

--- src/app/page.js
+++ src/app/page.js
@@ -2,6 +2,13 @@
 import Link from 'next/link'
 import Profile from '../../public/vercel.svg'
 import Image from 'next/image'
+import { Roboto } from 'next/font/google'
+
+const roboto = Roboto({
+  weight: '100',
+  subsets: ['latin'],
+  display: 'swap'
+})
 export default function Home() {
   return (
     <main>
@@ -9,7 +16,9 @@ export default function Home() {
       <Link href="/productlist">Go to Product List</Link>
       <br />
       <br />
+      <h1 style={{ fontFamily: 'Roboto', fontWeight: 100 }}>Font with link tag</h1>
       <Image src={Profile}></Image>
+      <h1 className={roboto.className}>Font with Next Js Font Feature</h1>
       <br />
       <br />
       <Image src="https://www.techgeekbuzz.com/media/tutorial_images/HTML-Images.jpg" height={200} width={400}></Image>
